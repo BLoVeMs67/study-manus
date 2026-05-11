8d3fa9d2156bc8d7a71a123377c7f739cddaa322
Bram <13675202119@163.com>
2026-05-11 19:51:41 +0800

    8-5 Postgres与Redis状态检查API的设计与开发

--- app/interfaces/endpoints/status_routes.py
+++ app/interfaces/endpoints/status_routes.py
@@ -2,9 +2,12 @@ import logging
 from typing import List
 
 from fastapi import APIRouter
+from fastapi.params import Depends
 
+from app.application.services.status_service import StatusService
 from app.domain.models.health_status import HealthStatus
 from app.interfaces.schemas import Response
+from app.interfaces.service_dependencies import get_status_service
 
 logger = logging.getLogger(__name__)
 router = APIRouter(prefix="/status", tags=["状态模块"])
@@ -16,7 +19,12 @@ router = APIRouter(prefix="/status", tags=["状态模块"])
     summary="系统健康检查",
     description="检查系统的postgres、redis、fastapi等组件的状态信息。"
 )
-async def get_status() -> Response:
+async def get_status(
+        status_service: StatusService = Depends(get_status_service)
+) -> Response:
     """系统健康检查，检查postgres/redis/fastapi/cos等服务"""
-    # TODO
-    return Response.success()
+    statuses = await status_service.check_all()
+
+    if any(item.status == "error" for item in statuses):
+        return Response.fail(503, "系统存在服务异常", statuses)
+    return Response.success(msg="系统健康检查成功", data=statuses)
--- app/interfaces/service_dependencies.py
+++ app/interfaces/service_dependencies.py
@@ -1,8 +1,16 @@
 import logging
 from functools import lru_cache
 
+from fastapi.params import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.application.services.app_config_service import AppConfigService
+from app.application.services.status_service import StatusService
+from app.infrastructure.external.health_checker.postgres_health_checker import PostgresHealthChecker
+from app.infrastructure.external.health_checker.redis_health_checker import RedisHealthChecker
 from app.infrastructure.repositories.file_app_config_repository import FileAppConfigRepository
+from app.infrastructure.storage.postgres import get_db_session
+from app.infrastructure.storage.redis import RedisClient, get_redis
 from core.config import get_settings
 
 logger = logging.getLogger(__name__)
@@ -19,3 +27,18 @@ def get_app_config_service() -> AppConfigService:
 
     # 2.实例化AppConfigService
     return AppConfigService(app_config_repository=file_app_config_repository)
+
+
+@lru_cache()
+def get_status_service(
+        db_session: AsyncSession = Depends(get_db_session),
+        redis_client: RedisClient = Depends(get_redis)
+) -> StatusService:
+    """获取状态服务"""
+    # 1.初始化postgres和redis健康检查
+    postgres_checker = PostgresHealthChecker(db_session)
+    redis_checker = RedisHealthChecker(redis_client)
+
+    # 2.创建服务并返回
+    logger.info("加载获取StatusService")
+    return StatusService(checkers=[postgres_checker, redis_checker])
