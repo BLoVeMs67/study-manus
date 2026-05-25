f3124118680a776049a2ecb77098a045131b6245
BLoVeMs67 <blovems67@gmail.com>
2026-05-25 15:14:58 +0800

    12-7 沙箱 异常

--- bram-manus/sandbox/app/main.py
+++ bram-manus/sandbox/app/main.py
@@ -7,6 +7,7 @@ from starlette.middleware.cors import CORSMiddleware
 
 from app.core.config import get_settings
 from app.interfaces.endpoints.file import router
+from app.interfaces.errors.exception_handler import register_exception_handlers
 
 
 def setup_logging() -> None:
@@ -89,5 +90,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# 5.集成路由
+# 5.注册错误并处理
+register_exception_handlers(app)
+
+# 6.集成路由
 app.include_router(router, prefix="/api")
