fc8ec585bafde157e04f7135ca265e5ac145cebf
BLoVeMs67 <blovems67@gmail.com>
2026-05-25 14:50:18 +0800

    12-6 沙箱FastAPI环境初始化

---
+++ bram-manus/sandbox/app/interfaces/endpoints/file.py
@@ -0,0 +1,4 @@
+from fastapi import APIRouter
+
+# 文件模块路由
+router = APIRouter(prefix="/file", tags=["文件模块"])
---
+++ bram-manus/sandbox/app/interfaces/endpoints/shell.py
@@ -0,0 +1,4 @@
+from fastapi import APIRouter
+
+# Shell模块路由
+router = APIRouter(prefix="/shell", tags=["Shell模块"])
